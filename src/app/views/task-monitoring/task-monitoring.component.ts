eb5e8822253acace435c2b163155da636c39ab01
glynn315 <94496602+glynn315@users.noreply.github.com>
2025-06-13 23:58:19 +0800

    Implement UI Menu and Modal

--- src/app/views/task-monitoring/task-monitoring.component.ts
+++ src/app/views/task-monitoring/task-monitoring.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule, AlarmClock } from 'lucide-angular';
 
 @Component({
   selector: 'app-task-monitoring',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './task-monitoring.component.html',
   styleUrl: './task-monitoring.component.scss'
 })
 export class TaskMonitoringComponent {
-
+  readonly AlarmClock = AlarmClock;
 }
